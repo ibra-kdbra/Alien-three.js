0977c1600cf715342c1a9a17935088957ef6df54
ibra-kdbra <sonarfarouq@gmail.com>
2023-05-08 22:58:52 +0300

    hero page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Navbar from './Navbar'
-import { styled } from 'styled-components'
+import { styled } from 'styled-components';
+// import { Canvas } from "@react-three/fiber";
+// import { OrbitControls, Sphere, MeshDistortMaterial } from "@react-three/drei";
 
 const Section = styled.div`
   height: 100vh;
@@ -18,7 +20,7 @@ const Section = styled.div`
 const Container = styled.div`
   height: 100%;
   scroll-snap-align: center;
-  width: 1400px;
+  width: 1200px;
   display: flex;
   justify-content: space-between;
 
@@ -122,12 +124,40 @@ const Img = styled.img`
 const Hero = () => {
   return (
     <Section>
-      <Navbar/>
+      <Navbar />
       <Container>
-      Hero
+        <Left>
+          <Title>Think. Make. Solve.</Title>
+          <WhatWeDo>
+            <Line src="./img/line.png" />
+            <Subtitle>What I Do</Subtitle>
+          </WhatWeDo>
+          <Desc>
+            I enjoy creating delightful, human-centered digital experiences.
+          </Desc>
+          <Button>Learn More</Button>
+        </Left>
+        <Right>
+          {/* <Canvas>
+            <Suspense fallback={null}>
+              <OrbitControls enableZoom={false} />
+              <ambientLight intensity={1} />
+              <directionalLight position={[3, 2, 1]} />
+              <Sphere args={[1, 100, 200]} scale={2.4}>
+                <MeshDistortMaterial
+                  color="#3d1c56"
+                  attach="material"
+                  distort={0.5}
+                  speed={2}
+                />
+              </Sphere>
+            </Suspense>
+          </Canvas> */}
+          <Img src="./img/moon.png" />
+        </Right>
       </Container>
     </Section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
