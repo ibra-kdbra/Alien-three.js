cf134451b44170af4e5c4ec9606a8a5774166b9c
ibra-kdbra <sonarfarouq@gmail.com>
2023-05-08 20:41:48 +0300

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
-function App() {
+import Hero from "./components/Hero";
+import Contact from "./components/Contact";
+import Who from "./components/Who";
+import Works from "./components/Works";
+import { styled } from "styled-components";
 
+const Container = styled.div`
+  height: 100vh;
+  scroll-snap-type: y mandatory;
+  scroll-behavior: smooth;
+  overflow-y: auto;
+  scrollbar-width: none;
+  color: white;
+  background: url("./img/bg.jpeg");
+  &::-webkit-scrollbar{
+    display: none;
+  }
+
+`
+
+function App() {
   return (
-    <div>
-      Hello world!
-    </div>
+    <Container>
+      <Hero/>
+      <Who/>
+      <Works/>
+      <Contact/>
+    </Container>
   )
 }
 
 export default App
+
+
