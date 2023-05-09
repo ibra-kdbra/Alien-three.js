47a504f512ec4829a71f546f74eeb51f03974c95
ibra-kdbra <sonarfarouq@gmail.com>
2023-05-10 01:00:00 +0300

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import Hero from "./components/Hero";
+import styled from "styled-components";
 import Contact from "./components/Contact";
+import Hero from "./components/Hero";
 import Who from "./components/Who";
 import Works from "./components/Works";
-import { styled } from "styled-components";
 
 const Container = styled.div`
   height: 100vh;
@@ -15,20 +15,17 @@ const Container = styled.div`
   &::-webkit-scrollbar{
     display: none;
   }
-
-`
+`;
 
 function App() {
   return (
     <Container>
-      <Hero/>
-      <Who/>
-      <Works/>
-      <Contact/>
+      <Hero />
+      <Who />
+      <Works />
+      <Contact />
     </Container>
-  )
+  );
 }
 
-export default App
-
-
+export default App;
