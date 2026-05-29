771496ef4aa415788e3bebbec06187c735284b9f
ibra-kdbra <git-repo-develop@outlook.com>
2026-05-29 23:32:57 +0300

    graphics: transform normal vectors to world-space in triplanar vertex shader

--- src/shaders/triplanar.vertex.glsl
+++ src/shaders/triplanar.vertex.glsl
@@ -2,7 +2,7 @@ varying vec3 vNormal;
 varying vec3 vWorldPosition;
 
 void main() {
-    vNormal = normalize(normalMatrix * normal);
+    vNormal = normalize((modelMatrix * vec4(normal, 0.0)).xyz);
     vec4 worldPosition = modelMatrix * vec4(position, 1.0);
     vWorldPosition = worldPosition.xyz;
     gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
